10785143b204dea844f3b8c286c552a80965fc41
David <lisaloerinci@gmail.com>
2021-09-14 20:38:35 +0200

    flake8 E226 correction

--- Source/Services/my_math_methods.py
+++ Source/Services/my_math_methods.py
@@ -3,13 +3,13 @@ import math
 
 class MyMethods():
     def addition(a, b):
-        return a+b
+        return a + b
 
     def pows(a, b):
         return math.pow(a, b)
 
     def substraction(a, b):
-        return a-b
+        return a - b
 
     def square( a ):
         return math.pow(a, 2)
