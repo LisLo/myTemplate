4b8e0d48f514cc4ea78c1d09d36b6e525ec8c8e9
David <lisaloerinci@gmail.com>
2021-09-14 20:36:09 +0200

    flake8 correction

--- Source/Services/my_math_methods.py
+++ Source/Services/my_math_methods.py
@@ -2,14 +2,14 @@ import math
 
 
 class MyMethods():
-    def addition( a, b ):
+    def addition(a, b):
         return a+b
 
-    def pows( a, b ):
-        return math.pow( a, b )
+    def pows(a, b):
+        return math.pow(a, b)
 
-    def substraction( a, b ):
+    def substraction(a, b):
         return a-b
 
     def square( a ):
-        return math.pow( a, 2 )
+        return math.pow(a, 2)
