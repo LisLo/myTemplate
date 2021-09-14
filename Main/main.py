3c536867b8f0dc49108d12ce1df98e0d0d7f68c7
David <lisaloerinci@gmail.com>
2021-09-14 20:30:34 +0200

    integration of Services

--- Main/main.py
+++ Main/main.py
@@ -5,13 +5,19 @@ root_path = os.path.split((os.path.dirname(__file__)))[0]
 # set root
 sys.path.append(root_path)
 from Source.Config.get_input import GetInput
+from Source.Services.my_math_methods import MyMethods
 
 
 class Main(GetInput):
     def execute_workflow(self):
-        print(self.a + self.b)
-        print(self.a * self.b)
-        print(self.a * self.a)
+        print("Addition:")
+        print(MyMethods.addition(self.a, self.b))
+        print("Substraction:")
+        print(MyMethods.substraction(self.a, self.b))
+        print("a hoch b")
+        print(MyMethods.pows(self.a, self.b))
+        print("Square")
+        print(MyMethods.square(self.a))
 
 
 my_code = Main(__file__)
